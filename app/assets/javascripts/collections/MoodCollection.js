7b5ee136e38adb4aed3ad04d70e2f68260a3f903
Ramona Bellamy <bellamy2727@yahoo.com>
2015-06-19 13:55:38 -0500

    setting up files

---
+++ app/assets/javascripts/collections/MoodCollection.js
@@ -0,0 +1,4 @@
+var MoodCollection = Backbone.Collection.extend({
+	model: MoodModel,
+	url: ''
+})
--- app/assets/javascripts/main.js.jsx
+++ app/assets/javascripts/main.js.jsx
@@ -1,17 +1,68 @@
+var containerEl =  document.querySelector('#container');
+
 /* global Backbone React */
 var App = Backbone.Router.extend({
     routes: {
-        '': 'profile',
-        'edit': 'edit'
+        '':                     'home',
+        'login':                'login',
+        'signup':               'signup',
+        'moods/:category':      'moods',
+        'playlist/:name':       'playlist',
+        'userplaylist/:name':   'userplaylist',
+        'profile/:name':        'profile'
+
+    },
+    home: function() {
+        console.log('home')
+        React.render(
+            <Home/>,
+            containerEl
+        );
+    },
+    login: function() {
+        console.log('login')
+        React.render(
+           <Login/>,
+            containerEl
+        );
+    },
+    signup: function() {
+        console.log('signup')
+        React.render(
+            <Signup/>,
+            containerEl
+        );
+    },
+    moods: function() {
+        console.log('moods')
+        React.render(
+            <Moods/>,
+            containerEl
+        );
+    },
+    playlist: function() {
+        console.log('playlist')
+        React.render(
+           <Playlist/>,
+            containerEl
+        );
     },
     profile: function() {
-      React.render(<Profile/>, document.querySelector('#container'));
+        console.log('profile')
+        React.render(
+           <Profile/>,
+            containerEl
+        );
     },
-    edit: function() {
-      React.render(<Edit/>, document.querySelector('#container'));
+    soundcloud: function(){
+        console.log('userplaylist')
+        React.render(
+            <Userplaylist/>,
+            containerEl
+        );
     }
 });
 
 var app = new App();
 Backbone.history.start();
-app.navigate('edit');
+// app.navigate('');

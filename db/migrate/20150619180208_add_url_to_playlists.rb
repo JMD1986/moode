8ccbfe5603b76c164ed31f96564dbfe17a9dff37
John Daugherty <johndaugherty1986@gmail.com>
2015-06-19 13:04:49 -0500

    added url:string to playlist table

--- db/migrate/20150619180208_add_url_to_playlists.rb
+++ db/migrate/20150619180208_add_url_to_playlists.rb
@@ -1,4 +1,5 @@
 class AddUrlToPlaylists < ActiveRecord::Migration
   def change
+    add_column :playlists, :url, :string
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150619175026) do
+ActiveRecord::Schema.define(version: 20150619180208) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(version: 20150619175026) do
     t.integer  "mood_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "url"
   end
 
   add_index "playlists", ["mood_id"], name: "index_playlists_on_mood_id", using: :btree

5c59c9025d30496c5dbf85b172af2c8d437ccced
John Daugherty <johndaugherty1986@gmail.com>
2015-06-19 21:06:45 -0500

    added user login logout routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,10 @@ Rails.application.routes.draw do
   resources :playlists
   root 'application#index'
 
+ get '/signup', to: "users#new"
 
+  get '/login', to: "sessions#new", as: 'login'
+  post '/login', to: "sessions#create", as: 'create_session'
+  get '/logout', to: "sessions#destroy", as: 'logout'
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,11 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-moods = Mood.create([{ mood: "hyper"}, {mood: "happy"}, {mood: "angry"}, { mood: "chill"}, { mood: "turnt"}, {mood: "anxious"}, {mood: "sensual"}, {mood: "silly"}, {mood: "tired "}])
+moods = Mood.create([{ mood: "In The Zone"}, {mood: "happy"}, {mood: "angry"}, { mood: "Relaxed"}, { mood: "Turnt"}, {mood: "Anxious"}, {mood: "Sensual"}, {mood: "Playful"}, {mood: "Tired"}])
 #password?!!
 
-playlists = Playlist.create([{ user: "John", title: "Turned up", url: "https://soundcloud.com/brainfeeder/thundercat-them-changes", mood_id: 1}])
+playlists = Playlist.create([{ user: "John", title: "Turned up", url: "https://soundcloud.com/brainfeeder/thundercat-them-changes", mood_id: 1},
+  { user: "John", title: "the house that funk built" , url: "https://soundcloud.com/amon-tobin/bedtime-stories-austin-peralta" , mood_id: 2},
+  { user: "Romona" , title: "Sometimes I eat bees", url: "https://soundcloud.com/ekalimusic/longtime" , mood_id: 3}, { user: "Tito" , title: "June good times playlist", url:"https://soundcloud.com/lil_jon/turn-down-for-what-dj-snake" , mood_id:5}])
 
+users = User.create([{ name: "John", email: "John@John.com", password_digest: "hello"}, {name: "Romona", email: "Ramona@rramona.com", password_digest: "hello"}, {name: "Tito", email: "Tito@ilikebigbutts.org", password_digest: "hello"}, {name: "Summer", email: "summer@google.com", password_digest: "hello"}, {name: "David", email: "David@hotmail.org", password_digest: "hello"}])

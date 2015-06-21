640cbf5b431661be07b57fb005e29b97b783c04c
John Daugherty <johndaugherty1986@gmail.com>
2015-06-20 21:44:05 -0500

    fixed submission form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
 
 
   def create
-    @user = User.new(email: params[:email], email: params[:email], password: params[:password], password_confirmation: params[:password_confirmation] )
+    @user = User.new(name: params[:name], email: params[:email],  password: params[:password], password_confirmation: params[:password_confirmation] )
 
     respond_to do |format|
       if @user.save

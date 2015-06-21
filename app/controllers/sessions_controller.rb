29b0c22bba3a60eb1d0ff571833d1e007873bf58
John Daugherty <johndaugherty1986@gmail.com>
2015-06-21 12:29:02 -0500

    fixing seeds

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,21 +3,21 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by_email(email: params[:email])
+    user = User.find_by_name(name: params[:name])
     if user && user.authenticate(password: params[:password])
     # user = User.find_by_email(email: params[:email])
     # if user && user.authenticate(password: params[:password])
       session[:user_id] = user.id
-      redirect_to root_url, notice: 'You logged in successfully! Hooray!'
+      render json: 'You logged in successfully! Hooray!'
     else
       flash[:alert] = "Username or email did not match."
-      render :new
+      render json: "Username or email did not match."
     end
   end
 
   def destroy
     session[:user_id] = nil
-    redirect_to root_url, notice: 'Successfully logged out.'
+    render json: "logged out"
   end
 
 end

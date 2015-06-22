201ce6bffe3e1c830fbbf2b9ddbb42e19a9d2660
John Daugherty <johndaugherty1986@gmail.com>
2015-06-21 21:23:40 -0500

    stressed out and making bonehead mistakes. fixed typo in playlist image url

--- db/seeds.rb
+++ db/seeds.rb
@@ -44,15 +44,15 @@ users = User.create([{
   imageurl:"http://www.teslasociety.com/pictures/pic1.jpg"}])
 
 playlists = Playlist.create(
-  [{ user: "John", user_id: 1, title: "Burning down the house", url: "https://soundcloud.com/maddecent/sets/jeffrees-vol-6", imageurl: "ttp://i.imgur.com/5063SUw.png", mood_id: 5},
-  { user: "John", user_id: 1, title: "the house that funk built" , url: "https://soundcloud.com/joshua-kong/sets/chillstep", imageurl: "ttp://i.imgur.com/5063SUw.png",mood_id: 1},
-  { user: "John", user_id: 1 ,title: "Ayy Lmao", url: "https://soundcloud.com/sarbs/sets/comedy-bang-bang", imageurl: "ttp://i.imgur.com/5063SUw.png",mood_id: 8},
-  { user: "Ramona", user_id: 2, title: "Coding mix", url: "https://soundcloud.com/victoria-feere/sets/coding-mix", imageurl: "ttp://i.imgur.com/5063SUw.png",mood_id: 4},
-  { user: "Ramona", user_id: 2, title: "May 2015 Austin Spring relaxation", url: "https://soundcloud.com/celestial_vibes/sets/summer-vibes-v2", imageurl: "ttp://i.imgur.com/5063SUw.png",mood_id: 1},
-  { user: "Tito" , user_id: 3, title: "June good times playlist", url: "https://soundcloud.com/ngryullet/sets/reggae", imageurl: "ttp://i.imgur.com/5063SUw.png",mood_id: 4},
-  { user: "Tito", user_id: 3, title: "I need a laugh", url: "https://soundcloud.com/mazemerizer/sets/standup", imageurl: "ttp://i.imgur.com/5063SUw.png",mood_id: 8},
-  { user: "Ramona", user_id: 3, title: "Night time tunes", url: "https://soundcloud.com/reecerance/sets/ambient-music", imageurl: "ttp://i.imgur.com/5063SUw.png",mood_id: 9},
-  { user: "Tesla", user_id: 4, title: "Thomas Eddison is an asshole", url: "https://soundcloud.com/mazemerizer/sets/standup", imageurl: "ttp://i.imgur.com/5063SUw.png", mood_id: 8},
+  [{ user: "John", user_id: 1, title: "Burning down the house", url: "https://soundcloud.com/maddecent/sets/jeffrees-vol-6", imageurl: "http://i.imgur.com/5063SUw.png", mood_id: 5},
+  { user: "John", user_id: 1, title: "the house that funk built" , url: "https://soundcloud.com/joshua-kong/sets/chillstep", imageurl: "http://i.imgur.com/5063SUw.png",mood_id: 1},
+  { user: "John", user_id: 1 ,title: "Ayy Lmao", url: "https://soundcloud.com/sarbs/sets/comedy-bang-bang", imageurl: "http://i.imgur.com/5063SUw.png",mood_id: 8},
+  { user: "Ramona", user_id: 2, title: "Coding mix", url: "https://soundcloud.com/victoria-feere/sets/coding-mix", imageurl: "http://i.imgur.com/5063SUw.png",mood_id: 4},
+  { user: "Ramona", user_id: 2, title: "May 2015 Austin Spring relaxation", url: "https://soundcloud.com/celestial_vibes/sets/summer-vibes-v2", imageurl: "http://i.imgur.com/5063SUw.png",mood_id: 1},
+  { user: "Tito" , user_id: 3, title: "June good times playlist", url: "https://soundcloud.com/ngryullet/sets/reggae", imageurl: "http://i.imgur.com/5063SUw.png",mood_id: 4},
+  { user: "Tito", user_id: 3, title: "I need a laugh", url: "https://soundcloud.com/mazemerizer/sets/standup", imageurl: "http://i.imgur.com/5063SUw.png",mood_id: 8},
+  { user: "Ramona", user_id: 3, title: "Night time tunes", url: "https://soundcloud.com/reecerance/sets/ambient-music", imageurl: "http://i.imgur.com/5063SUw.png",mood_id: 9},
+  { user: "Tesla", user_id: 4, title: "Thomas Eddison is an asshole", url: "https://soundcloud.com/mazemerizer/sets/standup", imageurl: "http://i.imgur.com/5063SUw.png", mood_id: 8},
 ])
 
 

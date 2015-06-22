c9d01ebdf6f663cf7fed95359508abc6417ed978
John Daugherty <johndaugherty1986@gmail.com>
2015-06-21 19:03:15 -0500

    db stuff

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,8 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 moods = Mood.create([{mood: "Happy", url: "http://streetdose.com/wp-content/uploads/2012/10/drake-smiling-after-graduating-high-school.jpg"},
-  {mood: "Confused", url: "http://replygif.net/thumbnail/1322.gif"},
-  {mood: "Angry", url: "http://myspiltmilk.com/sites/default/files/main-image/lewisblack_0.jpg"},
+  {mood: "Confused", url: "http://i.imgur.com/ESpXo1h.png"},
+  {mood: "Angry", url: "http://blog.afflictionclothing.com/wp-content/uploads/2014/11/james-hetfield.jpg"},
   {mood: "Relaxed", url: "http://www.greatamericanthings.net/wp-content/uploads/2012/03/Buffett-by-sweetslyricsdotcom.jpg"},
   {mood: "Turnt", url: "http://exytmusic.com/wp-content/uploads/2014/04/LilJon-1016x1024.jpg"},
   {mood: "Anxious", url: "http://cdn.ustatik.com/_img/columns/Jonathan-Davis.jpg"},
@@ -22,13 +22,13 @@ playlists = Playlist.create(
   { user: "John", title: "the house that funk built" , url: "https://soundcloud.com/joshua-kong/sets/chillstep", mood_id: 1},
   { user: "John", title: "Ayy Lmao", url: "https://soundcloud.com/sarbs/sets/comedy-bang-bang", mood_id: 8},
   { user: "Ramona", title: "Coding mix", url: "https://soundcloud.com/victoria-feere/sets/coding-mix", mood_id: 4},
-  { user: "Romona" , title: "May 2015 Austin Spring relaxation", url: "https://soundcloud.com/celestial_vibes/sets/summer-vibes-v2", mood_id: 1},
+  { user: "Ramona" , title: "May 2015 Austin Spring relaxation", url: "https://soundcloud.com/celestial_vibes/sets/summer-vibes-v2", mood_id: 1},
   { user: "Tito" , title: "June good times playlist", url: "https://soundcloud.com/ngryullet/sets/reggae", mood_id: 4},
   { user: "Tito", title: "I need a laugh", url: "https://soundcloud.com/mazemerizer/sets/standup", mood_id: 8},
   { user: "Ramona", title: "Night time tunes", url: "https://soundcloud.com/reecerance/sets/ambient-music", mood_id: 9}])
 
 users = User.create([{ name: "John", email: "Johndaugherty1986@gmail.com", imageurl: "http://i.imgur.com/tciLfzQ.jpg"},
-  {name: "Romona", email: "bellamy2727@yahoo.com",  imageurl: "http://i.imgur.com/5A95Faw.jpg"},
+  {name: "Ramona", email: "bellamy2727@yahoo.com",  imageurl: "http://i.imgur.com/5A95Faw.jpg"},
   {name: "Tito", email: "Tito@ilikebigbutts.org", imageurl: "http://i.imgur.com/4PgOxLr.jpg"},
   {name: "Nikola Tesla", email: "Ilovescience@electricityisdope.com", imageurl:"http://www.teslasociety.com/pictures/pic1.jpg"}])
 
